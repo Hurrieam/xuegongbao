8271a67b8bddf38a3a2d1a16dd11324eeb3f91ce
Zolo <zolo@onezol.com>
2022-04-23 23:14:40 +0800

    pref: exclude typescript files from the final project

--- util/copier.ts
+++ util/copier.ts
@@ -2,6 +2,16 @@ import path from 'path';
 import fs from 'fs';
 
 /**
+ * 删除目录
+ * @param filePath
+ */
+const removeFile = (filePath: string) => {
+    if (fs.existsSync(filePath)) {
+        fs.rmSync(filePath, {recursive: true});
+    }
+};
+
+/**
  * 将sourcePath目录下的文库全部复制到targetPath目录下
  * @param sourcePath  源路径
  * @param targetPath  目标路径
@@ -10,13 +20,15 @@ const copyFile = (sourcePath: string, targetPath: string) => {
     // 读取sourcePath目录下的文件列表
     const files = fs.readdirSync(sourcePath);
     files.forEach((file: string) => {
-        // 获取当前文件的绝对路径
         const currentSourcePath = path.join(sourcePath, file);
-        // 获取目标文件的绝对路径
         const currentTargetPath = path.join(targetPath, file);
         // 判断当前文件是否是目录
         if (fs.statSync(currentSourcePath).isDirectory()) {
-            copyFile(currentSourcePath, currentTargetPath);
+            // 最终项目中排除ts文件
+            if (!currentSourcePath.endsWith(path.sep + "ts")) {
+                fs.mkdirSync(currentTargetPath);
+                copyFile(currentSourcePath, currentTargetPath);
+            }
         } else {
             // 当前文件不是目录，则复制
             fs.copyFileSync(currentSourcePath, currentTargetPath);
@@ -27,8 +39,8 @@ const copyFile = (sourcePath: string, targetPath: string) => {
 (() => {
     const sourcePath = path.resolve(__dirname, '../../static');
     const targetPath = path.resolve(__dirname, '../static');
-    if (!fs.existsSync(targetPath)) {
-        fs.mkdirSync(targetPath);
-    }
+
+    removeFile(targetPath);
+    fs.mkdirSync(targetPath);
     copyFile(sourcePath, targetPath);
 })();
